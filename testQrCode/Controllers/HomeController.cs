b84cbbe646781b3b29c965c9155502fa1df6991a
cash <cash@cashwu.com>
2022-01-11 16:59:29 +0800

    add another package

--- testQrCode/Controllers/HomeController.cs
+++ testQrCode/Controllers/HomeController.cs
@@ -1,38 +1,64 @@
-﻿using System.Drawing.Imaging;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using SkiaSharp;
+using SkiaSharp.QrCode;
 
 namespace testQrCode.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IQrCode _qrCode;
-
-        public HomeController(IQrCode qrCode)
-        {
-            _qrCode = qrCode;
-        }
-
         public IActionResult Index()
         {
             return Content("ok");
         }
 
         /// <summary>
-        /// http://localhost:40144/api/qrcode?url=http://blog.cashwu.com&pixel=15
+        /// http://localhost:40144/qrcode?url=http://blog.cashwu.com&pixel=15
         /// </summary>
         /// <param name="url"></param>
         /// <param name="pixel"></param>
         /// <returns></returns>
-        [HttpGet("/api/qrcode")]
-        public FileContentResult GetQrCode(string url, int pixel)
+        [HttpGet("/qrcode")]
+        public FileContentResult GetQrCode(string url, int pixel = 15)
         {
-            var bitmap = _qrCode.Get(url, pixel);
+            // var bitmap = _qrCode.Get(url, pixel);
+            var qrCodeGenerator = new QRCoder.QRCodeGenerator();
+            var qrCodeData = qrCodeGenerator.CreateQrCode(url, QRCoder.QRCodeGenerator.ECCLevel.M, true);
+            var qrCode = new QRCode(qrCodeData);
+            var bitmap = qrCode.GetGraphic(pixel, Color.Black, Color.White, true);
 
             var ms = new MemoryStream();
             bitmap.Save(ms, ImageFormat.Png);
 
             return File(ms.GetBuffer(), "image/png");
+        }
+
+        /// <summary>
+        /// ref https://khalidabuhakmeh.com/generate-qr-codes-csharp
+        /// 
+        /// http://localhost:40144/qrcode2?url=http://blog.cashwu.com
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        [HttpGet("/qrcode2")]
+        public FileContentResult SaveQrCode(string url)
+        {
+            var info = new SKImageInfo(512, 512);
+            using var surface = SKSurface.Create(info);
+
+            var qr = new SkiaSharp.QrCode.QRCodeGenerator().CreateQrCode(url, ECCLevel.H);
+            surface.Canvas.Render(qr, SKRect.Create(512, 512), 512, SKColors.Black);
+
+            using var data = surface.Snapshot().Encode(SKEncodedImageFormat.Png, 100);
+
+            return File(data.ToArray(), "image/png");
+            
+            // save to file
+            // using var stream = System.IO.File.OpenWrite(@$"qr-{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.png");
+            // data.SaveTo(stream);
         }
     }
 }
--- testQrCode/Startup.cs
+++ testQrCode/Startup.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace testQrCode
 {
@@ -23,21 +17,10 @@ namespace testQrCode
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            services.AddScoped<IQrCode, RaffQrCode>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
             app.UseStaticFiles();
 
             app.UseRouting();
